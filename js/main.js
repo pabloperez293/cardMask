cba8d4514fa468033ea39c0791a035a5668df9a7
pabloperez293 <paperezpablo@gmail.com>
2023-07-27 08:56:00 -0300

    dateNumber

--- js/main.js
+++ js/main.js
@@ -8,10 +8,12 @@ const maskDate = "##-##";
 const maskCVV = "###";
 
 let current = "";
+// Arreglso vacios
 let cardNumber = [];
 let dateNumber = [];
 let cvvNumber = [];
 
+// teclas de nuestro teclado
 inputCard.addEventListener("keydown", (evt) =>{
     if(evt.key === "tab"){
         return;
@@ -20,3 +22,32 @@ inputCard.addEventListener("keydown", (evt) =>{
     handleInput(maskNumber, evt.key, cardNumber); //va a pedir el numero, con la clave y la tarjeta
     inputCard.value = cardNumber.join("");
 });
+
+inputDate.addEventListener("keydown", (e) =>{
+    if(e.key === "tab"){
+        return;
+    }
+    e.preventDefault();
+    handleInput()
+
+})
+
+
+
+// recibo las mask y ya defino mis numeros linea 27
+function handleInput(mask, key, arr){
+    let numbers =["0","1","2","3","4","5","6","7","8","9"];
+// simulan quitar un caracter
+    if(key === "Backspace" && arr.length > 0){
+       arr.pop();
+       return;
+    }
+// validamos si la tecla ingresada exite en el let de number de ahi medimos con el lenght
+    if(numbers.includes(key) && arr.length +1 <= mask.length){
+        if(mask[arr.length] === "-" || mask[arr.length] === "/"){
+            arr.push(mask[arr.length], key);
+        }else{
+            arr.push(key);
+        }
+    }
+}
